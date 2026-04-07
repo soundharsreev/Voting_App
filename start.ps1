b5bc23a55c892e721342fdf25827c79d8440b7c4
soundharsreev <sreesoundhar7@gmail.com>
2026-04-07 23:13:34 +0530

    Delete start.ps1

--- start.ps1
+++
@@ -1,3 +0,0 @@
-Start-Process powershell -ArgumentList "-NoExit -Command `"cd backend; npm run dev`""
-Start-Process powershell -ArgumentList "-NoExit -Command `"cd frontend; npm run dev`""
-Write-Host "Services started in new windows!"
